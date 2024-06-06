1b882d9fd4f2cc9b581d2771cfdf68ad62fe89c8
Jason Loeffler <jason.loeffler@xplortechnologies.com>
2024-06-06 12:57:50 -0500

    Make parameters easier to work with

--- Coralogix.psm1
+++ Coralogix.psm1
@@ -1,39 +1,38 @@
+enum LogSeverity {
+    Debug = 1
+    Verbose = 2
+    Info = 3
+    Warn = 4
+    Error = 5
+    Critical = 6
+}
+
 Function Write-CoralogixLog {
     param(
-        [string] $applicationName, 
-        [string] $subsystemName, 
-        [string] $computerName, 
-        [int] $severity, 
+        [string] $ApplicationName = [System.Diagnostics.Process]::GetCurrentProcess().ProcessName, 
+        [string] $SubsystemName = [System.Diagnostics.Process]::GetCurrentProcess().Product, 
+        [string] $ComputerName = $env:COMPUTERNAME, 
+        [LogSeverity] $Severity = [LogSeverity]::Info, 
         [Parameter(Mandatory = $true)]
-        [string] $text, 
-        [string] $cxDomain,
-        [string] $cxApiKey)
-
-    if ($severity -eq 0) { 
-        $severity = 3 
-    }
-
-    if ($cxDomain -eq $null) {
-        $cxDomain = $env:CX_DOMAIN
-    }
-
-    if ($cxApiKey -eq $null) {
-        $cxApiKey = $env:CX_API_KEY
-    }
+        [string] $Text, 
+        [string] $CxDomain = $env:CX_DOMAIN,
+        [string] $CxApiKey = $env:CX_API_KEY)
 
     $body = @{
-        "applicationName" = $applicationName
-        "subsystemName"   = $subsystemName
-        "computerName"    = $computerName
-        "severity"        = $severity
-        "text"            = $text
+        "applicationName" = $ApplicationName
+        "subsystemName"   = $SubsystemName
+        "computerName"    = $ComputerName
+        "severity"        = $Severity
+        "text"            = $Text
     }
 
     $bodyDocument = $body | ConvertTo-Json -AsArray
 
+    Write-Verbose $bodyDocument
+
     try {
-        Invoke-RestMethod -Uri "https://ingress.$cxDomain/logs/v1/singles" `
-            -Headers @{ "Authorization" = "Bearer $cxApiKey" } `
+        Invoke-RestMethod -Uri "https://ingress.$CxDomain/logs/v1/singles" `
+            -Headers @{ "Authorization" = "Bearer $CxApiKey" } `
             -ContentType "application/json" `
             -Method Post `
             -Body $bodyDocument | Out-Null
